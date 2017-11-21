813f952ecd22b16babc81c43ff3a117ae3814ada
Serghei Filippov <serghei.filippov@gmail.com>
2017-11-20 17:01:21 -0700

    Haskell: Adds linter

---
+++ SublimeLinter-ghc/linter.py
@@ -0,0 +1,54 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Jon Surrell
+# Copyright (c) 2013 Jon Surrell
+#
+# License: MIT
+#
+
+"""This module exports the Ghc plugin class."""
+
+from SublimeLinter.lint import Linter, util
+from os.path import basename
+
+
+class Ghc(Linter):
+
+    """Provides an interface to ghc."""
+
+    syntax = ('haskell', 'haskell-sublimehaskell', 'literate haskell')
+    cmd = ('ghc', '-fno-code', '-Wall', '-Wwarn', '-fno-helpful-errors')
+    regex = (
+        r'(?i)'
+        r'^(?P<filename>.+):'
+        r'(?P<line>\d+):(?P<col>\d+):'
+        r'\s+(?P<warning>Warning:\s+)?(?P<message>([\n]|.)+?)(?=\n\s*\|)'
+    )
+    multiline = True
+
+    # No stdin
+    tempfile_suffix = {
+        'haskell': 'hs',
+        'haskell-sublimehaskell': 'hs',
+        'literate haskell': 'lhs'
+    }
+
+    # ghc writes errors to STDERR
+    error_stream = util.STREAM_STDERR
+
+
+    def split_match(self, match):
+        """Override to ignore errors reported in imported files."""
+        match, line, col, error, warning, message, near = (
+            super().split_match(match)
+        )
+
+        match_filename = basename(match.groupdict()['filename'])
+        linted_filename = basename(self.filename)
+
+        if match_filename != linted_filename:
+            return None, None, None, None, None, '', None
+
+        return match, line, col, error, warning, message, near
